03a685b1155b90d1a1204ee7ac245035dd67a789
Abhishek Jha <jhaabhishek910@gmail.com>
2025-02-22 16:03:38 +0530

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="font-bold rose-500">
-      Hello world
+    <div>
+      <Image src="/logo.svg" height={50} width={50} alt="logo"/>
+      <p className="text-xl font-semibold tracking-tight">YouTube</p>
+      <Button onClick={() => router.push("/feed")}>
+        Click Me
+      </Button>
     </div>
   );
 }
